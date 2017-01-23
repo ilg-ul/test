b6495b8204396af787debca8193224de35330ea2
Liviu Ionescu <ilg@livius.net>
2017-01-23 11:33:34 +0200

    separate travis.osx.sh

---
+++ scripts/travis.osx.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+
+pwd
+uname -a
+clang --version
+java -version
+# cat /Users/travis/build.sh
+env
+id
+who am i
+whoami
+who
+
+brew --version
+brew tap homebrew/versions
+brew install gcc5
+
